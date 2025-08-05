fd2d7e3f26d9b22bdac1239d6178da47ec115b77
maji <maji@himaji.xyz>
2025-08-05 21:51:01 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import { connectDevice, getAdb, requestDevice } from './utils/device';
+import type { Adb, AdbSync } from '@yume-chan/adb';
+import path from 'path-browserify';
+import { IconCardboards, IconMusic } from '@tabler/icons-vue';
+
+const CUSTOM_SONG_LOCATION = "storage/emulated/0/Android/data/com.xrgames.beatable/files/CustomSongs"
+
+const currentDeviceInfo = ref()
+const deviceConnected = ref(false)
+const uploadingSong = ref(false)
+const uploadingName = ref("")
+const connectionState = ref("")
+const installedCharts: Ref<IChartFile[]> = ref([])
+let adb: Adb
+let sync: AdbSync
+
+interface IChartFile {
+  filePath: string
+  fileName: string
+}
+
+const readChartsInDevice = async () => {
+  installedCharts.value = []
+  const customSongs = await sync.readdir(CUSTOM_SONG_LOCATION)
+
+  for (const song of customSongs) {
+    if (song.name.endsWith(".beats")) {
+      installedCharts.value.push({
+        fileName: song.name,
+        filePath: path.join(CUSTOM_SONG_LOCATION, song.name)
+      })
+    }
+  }
+}
+
+const openDevice = async () => {
+  const device = await requestDevice()
+  if (device) {
+    console.log(device)
+    currentDeviceInfo.value = device.raw
+    if (device.raw.manufacturerName != "Oculus") {
+      alert("Connected device is not Meta Quest")
+      return
+    }
+    try {
+      connectionState.value = "connecting"
+      console.log("Connecting...")
+      const connection = await connectDevice(device)
+      connectionState.value = "authenticating"
+      console.log("Authenticating...")
+      adb = await getAdb(device, connection)
+
+      if (adb) {
+        console.log(adb)
+        sync = await adb.sync();
+
+        console.log(sync)
+        const appFiles = await sync.readdir("storage/emulated/0/Android/data/")
+        console.log(appFiles)
+        const beatableInstalled = appFiles.find((e) => e.name == "com.xrgames.beatable") != undefined
+          connectionState.value = ""
+
+        if (beatableInstalled) {
+          console.log("Beatable installed")
+          readChartsInDevice()
+          deviceConnected.value = true
+        } else {
+          alert("Beatable is not installed")
+        }
+        //const lsData = await runCmd("cd storage/emulated/0/Android/data/ && ls -1")
+        //console.log(lsData)
+      }
+    } catch (err) {
+      alert(err)
+    }
+  }
+}
+
+const installChart = async (event: any) => {
+  uploadingSong.value = true
+  for (const file of event.target.files) {
+    console.log("Uploading ", file.name)
+    console.log(file)
+    uploadingName.value = file.name
+
+    await sync.write({
+      filename: path.join(CUSTOM_SONG_LOCATION, file.name),
+      file: file.stream()
+    })
+  }
+  uploadingSong.value = false
+  readChartsInDevice()
+}
+
+const deleteFile = async (chartFileData: IChartFile) => {
+  await adb.rm(chartFileData.filePath)
+  console.log(chartFileData.fileName, " Deleted")
+  readChartsInDevice()
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="banner">
+      <h1>BEATABLE Soundtrack Manager</h1>
+      <p>Manage your BEATABLE soundtracks, on the web</p>
+    </div>
+    <main>
+      <div>
+        <div v-if="deviceConnected" class="d-flex align-items-center">
+          <IconCardboards size="48" stroke-width="1" />
+          <div class="ms-2">
+            <p class="text-muted m-0">Connected Device</p>
+            <h3 class="m-0">{{ currentDeviceInfo.productName }}</h3>
+          </div>
+        </div>
+      </div>
+      <div v-if="!deviceConnected">
+        <h3>Connect your device</h3>
+        <p>Connect your Quest to your computer, then click 'Select Device' and choose your Quest from the list.</p>
+        <button class="btn btn-light" @click="openDevice">Select Device</button>
+      </div>
+      <div v-if="connectionState == 'authenticating'">
+        <p>Put on your headset then press "Allow" to continue</p>
+      </div>
+    </main>
+
+    <div v-if="deviceConnected">
+      <hr>
+      <h3>Installed Soundtracks</h3>
+
+      <div class="card soundtrack-card mb-1" v-for="f in installedCharts">
+        <div class="card-body">
+          <p>
+            <IconMusic class="me-1" stroke-width="1.5" /> {{ f.fileName }}
+          </p> <button class="btn btn-danger" @click="deleteFile(f)">Delete</button>
+        </div>
+      </div>
+
+      <p class="text-muted" v-if="installedCharts.length == 0">No custom songs installed -w-</p>
+
+      <hr>
+
+      <h3>Upload Soundtrack to Device</h3>
+
+      <input v-if="!uploadingSong" class="form-control" type="file" multiple @change="installChart" accept=".beats" />
+      <div v-if="uploadingSong" class="d-flex align-items-center mt-2">
+        <div class="spinner-border text-light me-2" role="status"></div>
+        <p>Uploading {{ uploadingName }}</p>
+      </div>
+    </div>
+
+    <div>
+      <hr>
+      <p>Made by <a href="https://himaji.xyz/">himaji!</a></p>
+      <p>Made by <a href="https://himaji.xyz/">Source code</a></p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/utils/device.ts
@@ -0,0 +1,33 @@
+import AdbWebCredentialStore from "@yume-chan/adb-credential-web";
+import { AdbDaemonWebUsbConnection, AdbDaemonWebUsbDevice, AdbDaemonWebUsbDeviceManager } from "@yume-chan/adb-daemon-webusb";
+import { Adb, AdbDaemonTransport } from "@yume-chan/adb";
+
+const Manager: AdbDaemonWebUsbDeviceManager | undefined = AdbDaemonWebUsbDeviceManager.BROWSER;
+const CredentialStore: AdbWebCredentialStore = new AdbWebCredentialStore("BeatableWebInstaller");
+
+export async function requestDevice() {
+    if (!Manager) {
+        alert("Error: No ADB Interface")
+        return
+    }
+    const device: AdbDaemonWebUsbDevice | undefined = await Manager.requestDevice();
+    return device
+}
+
+export async function connectDevice(device: AdbDaemonWebUsbDevice) {
+    try {
+        return await device.connect();
+    } catch (error) {
+        throw error;
+    }
+
+}
+export async function getAdb(device: AdbDaemonWebUsbDevice, connection: AdbDaemonWebUsbConnection) {
+    const transport = await AdbDaemonTransport.authenticate({
+        serial: device.serial,
+        connection,
+        credentialStore: CredentialStore,
+    });
+
+    return new Adb(transport);
+}
